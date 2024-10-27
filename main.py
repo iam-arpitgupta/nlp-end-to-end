1c685999cb135c4a2a00a7c4d11ee6882357349e
Arpit <thearpitgupta2003@gmail.com>
2024-10-27 13:51:53 +0530

    tested the model on google collab

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.textSummarizer.logging import logger
+
+logger.info("Welcome to the Text Summarizer project")
+logger.info("This is the main entry point of the application")
+logger.info("Initializing the project...")
+    
+    # Add your main application logic here
+    
+logger.info("Application execution completed")
